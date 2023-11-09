907d525440c49d87016cf5145bd8b3a575932705
Mendes44 <marcosmendesm10@gmail.com>
2023-11-09 17:03:33 -0300

    Primeira Classe - OK

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,23 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("TESTE!!!!");
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
+		
+		
+		
+		sc.close();
 
 	}
 
