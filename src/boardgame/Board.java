7c2fee15849a85548ff871dfd232e7db9135e1bc
Mendes44 <marcosmendesm10@gmail.com>
2023-11-10 12:18:46 -0300

    Criação da BoardException e Programação Defensiva

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,11 @@ public class Board {
 	private Piece[][]pieces;
 	
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1 ) {
+			throw new BoardException("ERRO CRIANDO TABULEIRO! -> E NECESSARIO "
+					+ "QUE HAJA PELO MENOS 1 LINHA E 1 COLUNA. ");
+		}
+		
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -16,33 +21,53 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 	
-	//Criar um metodo Board.Piece (row, column) e Board.Pieces(position) = sobrecarga
 	
+	//Criar um metodo Board.Piece (row, column) e Board.Pieces(position)
+	//PROGRAMAÇÃO DEFENSIVA - TESTAR PARA VER SE NÃO EXISTE PEÇA NA MESMA POSIÇÃO.
 	public Piece piece(int row, int column) {
+		if (!positionExists(row,column)) {
+			throw new BoardException(">>> POSIÇÃO NÃO EXISTE NO TABULEIRO <<<");
+		}
 		return pieces [row][column];
 	}
 	
 	//Sobrecarga - Retorna a peça pela posição - Para pegar as peças na posição na matriz.
 	public Piece piece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException(">>> POSIÇÃO NÃO EXISTE NO TABULEIRO <<<");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece (Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException(">>> JA EXISTE UMA PEÇA NA POSIÇÃO -> " 
+		+ "LINHA:" + position.getRow()+ "--COLUNA:" + position.getColumn());
+		}
 		pieces [position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
-		
 	}
 	
+	/*PARA SABER SE JA EXISTE UMA PEÇA NO TABULEIRO.
+	 * FIZ UM METODO AUXILIAR PRIVATE  PARA FICAR MAIS FACIL DE TESTAR PELA LINHA E COLUNA
+	*/
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(),position.getColumn());//Estou buscando a posição do meu metodo auxiliar.
+	}
+	
+	public boolean thereIsAPiece (Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException(">>> POSIÇÃO NÃO EXISTE NO TABULEIRO <<<");
+		}
+		return piece(position) != null; //Se a peça for diferente de nulo e pq tem uma peça.
+	}
 }
