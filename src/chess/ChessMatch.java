672a7768b56ad44467a26e2f527d97291cbd9d0a
Mendes44 <marcosmendesm10@gmail.com>
2023-11-10 16:16:52 -0300

    Pequena Melhoria na Impressão do Tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -40,8 +40,18 @@ public class ChessMatch {
 	
 	//Aqui estou chamando o board para criar minhas peças na tela.
 	private void initialSetup() {
-		placeNewPiece('b',6, new Rook (board, Color.WHITE));//Estou falando para colocar na posição B6
-		placeNewPiece('e',8, new King (board, Color.BLACK));
-		placeNewPiece('e',1, new King (board, Color.WHITE));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
 }
